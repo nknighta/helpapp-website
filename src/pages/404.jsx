9cf35c2eaa2e4bf5d8d4daa2c20068ffc84442e5
nknighta <n.knight.pc0627@gmail.com>
2024-12-02 14:49:52 +0900

    fix: 🐛 update api

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function NOFOUND() {
     return (
         <>
             <div>
                 <p>ページが見つかりませんでした</p>
+                <Link href="/">
+
+                    <a>トップページに戻る</a>
+                </Link>
             </div>
         </>
     )
