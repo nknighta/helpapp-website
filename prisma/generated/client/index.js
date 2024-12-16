5c9156370405aee8562d0aabacf7d4e0451eda97
nknighta <n.knight.pc0627@gmail.com>
2024-12-16 12:42:35 +0900

    fix: 🐛 nextjs config

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -167,7 +167,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
